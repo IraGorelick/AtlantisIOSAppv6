f543949de38760b369b35cadc84f3d2ca3e3c903
IraGorelick <iragorelick@atlantislearningcommunity.org>
2020-11-25 07:17:42 -0500

    updated

--- AtlantisIOSAppv6/ViewController.swift
+++ AtlantisIOSAppv6/ViewController.swift
@@ -51,9 +51,16 @@ class ViewController: UIViewController {
         
     ]
 
+    var jsonTest = [
+        "date":"2020-10-16T14:09:56",
+        "date_gmt":"2020-10-16T18:09:56",
+        "modified":"2020-10-16T14:09:56",
+        "modified_gmt":"2020-10-16T18:09:56",
+        "slug":"racism-exists",
+        "status":"publish",
+        "type":"arguments",
+        ]
     
-    
-    var extraSyntax1: Array<String> = Array<String>()
     var testURL = "https://atlantisschoolofcommunication.org/wp-json/wp/v2/arguments"
     var newurl = URL.init(string: "https://atlantisschoolofcommunication.org/wp-json/wp/v2/arguments")
 
@@ -64,24 +71,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         Arguments.fetch()
         
-        print(tempInsert + "  - hslslsl")
-        print(tempInsert.count)
-        print(testArray.count)
-        print(testArray.isEmpty)
-        testArray[3] = "new"
-        print(testArray[1] + "hello")
-        
-        testArray.append("First W")
-        testArray.insert("new Insert", at: 2)
-        title = "actionable"
-        
-        
       
         var testArray2 = beliefTest["Harris"]?["Belief2"]
-        
+        var testArray3 = jsonTest["slug"]
         
         // Print results
         textWindow.text = testArray2
+        textLine1.text = "hello \(testArray3!)"
         print(testArray)
         print(beliefTest)
         print(testArray2)
@@ -92,7 +88,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func ButtonTest1(_ sender: Any) {
-        textLine1.text = "hello \(beliefTest)"
+        textLine1.text = "hello \(testURL)"
     }
     
     }
